d714c90917b59610d012509b93c1365b39feda72
KrzysztofGąciarz <59824733+szampeq@users.noreply.github.com>
2021-04-04 19:01:33 +0200

    steps data simplified

--- controllers/steps.js
+++ controllers/steps.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
@@ -22,25 +23,37 @@ exports.monthList =  async (req, res) => {
 exports.userData =  async (req, res) => {
     const userID = req.params.userid;
     let userdata = [];
-    await db.query('SELECT SUM(amount) AS weeksteps FROM steps INNER JOIN users ON steps.userid = users.id WHERE users.id = ? && day >= ( CURDATE() - INTERVAL 7 DAY )', userID, (error, results) => {
+    let weekscore = 0, monthscore = 0, allscore = 0;
+    const date = new Date();
+    date.setHours(0,0,0);
+    date.setUTCHours(0);
+    const weekStamp = 7 * 24 * 60 * 60 * 1000;
+    const monthStamp = 30 * 24 * 60 * 60 * 1000;
+
+
+    await db.query('SELECT day, amount FROM steps INNER JOIN users ON steps.userid = users.id WHERE users.id = ?', userID, (error, results) => {
         if (error) {
             throw error;
         }
+        console.log("date", date);
+        results.forEach(element => {
+            element.day.setHours(0, 0, 0);
+            element.day.setUTCHours(0);
+            if ((date - element.day) < weekStamp)
+                weekscore += element.amount;
+            if ((date - element.day) < monthStamp) {
+                monthscore += element.amount;
+            }
+            allscore += element.amount;
+        });
         userdata.push(results);
-    });
-    await db.query('SELECT SUM(amount) AS monthsteps FROM steps INNER JOIN users ON steps.userid = users.id WHERE users.id = ? && day >= ( CURDATE() - INTERVAL 30 DAY )', userID, (error, results) => {
-        if (error) {
-            throw error;
-        }
-        userdata.push(results);
-    });
-    await db.query('SELECT SUM(amount) AS allsteps FROM steps INNER JOIN users ON steps.userid = users.id WHERE users.id = ?', userID, (error, results) => {
-        if (error) {
-            throw error;
-        }
-        userdata.push(results);
+
         return res.send({
-            steps: userdata
+            steps: userdata,
+            weekscore: weekscore,
+            monthscore: monthscore,
+            allscore: allscore
         });
     });
+
 }
