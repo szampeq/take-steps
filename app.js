4db75e10e771f50b2c81867467d28ba2b66d7379
KrzysztofGąciarz <59824733+szampeq@users.noreply.github.com>
2021-04-05 23:47:20 +0200

    sql pool

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ dotenv.config({path: './.env'})
 
 const app = express();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
